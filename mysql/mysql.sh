6489db45063b5c865f64e13be45cfc91ccb6e513
Ivan Kaptue <ivanokaptue@gmail.com>
2022-04-30 16:46:52 -0400

    Fix mysql.sh, to add parameters DATABASE_ALREADY_EXISTS to prevent startup error for database already exists

---
+++ mysql/mysql.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# docker build -t klid/mysql:5.7 .
+
+docker run --name mysql5.7 \
+  -v ~/docker-data/mysql:/var/lib/mysql \
+  --user 1000:1000 \
+  -e MYSQL_ROOT_PASSWORD=root \
+  -p 3306:3306 \
+  -d klid/mysql:5.7
+
+#  -e MYSQL_ROOT_PASSWORD=root \
+#  -e DATABASE_EXISTS=true \
