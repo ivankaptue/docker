f68d20ba50d2e3eddd48818d3a77b916d0fb648a
Ivan Kaptue <ivanokaptue@gmail.com>
2023-01-15 22:39:23 -0500

    Update mysql config

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -7,7 +7,7 @@ docker run --name mysql5.7 \
   --user 1000:1000 \
   -e MYSQL_ROOT_PASSWORD=root \
   -p 3306:3306 \
-  -d klid/mysql:5.7
+  -d klid/mysql:5.7 --lower_case_table_names=1
 
 #  -e MYSQL_ROOT_PASSWORD=root \
 #  -e DATABASE_EXISTS=true \
